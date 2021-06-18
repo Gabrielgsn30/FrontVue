30fd97dfa517f10193f0d90377530edf70a0b74d
gabrielnascimentosilva <gabriel.nascimento@soitic.com>
2021-06-18 14:19:23 -0300

    First commit app ja trazendo os Diretores usando a api

---
+++ frontvue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Diretores msg="Front Usando WebapiDotnet"/>
+  </div>
+</template>
+
+<script>
+import Diretores from './components/Diretores.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Diretores
+  }
+}
+</script>
+
---
+++ frontvue/src/components/Diretores.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>
+            id
+          </th>
+          <th>
+            Nome
+          </th>
+          <th>
+            Filmes
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="diretor in diretores" v-bind:key="diretor.id">
+          <td>
+            {{diretor.id}}
+          </td>
+          <td>
+            {{diretor.nome}}
+          </td>
+          <td>
+            {{diretor.filmes}}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'Diretores',
+  props: {
+    msg: String
+  },
+  data:() =>{
+    return{
+    diretores:[],
+  }
+  },
+  methods: {
+    lista: (scope) =>{
+      axios.get(`https://localhost:5001/Diretor`).then((res)=>{
+      console.log(res)
+      scope.diretores = res.data
+    })
+  }
+},
+
+  created(){
+    this.lista(this)
+  }
+  
+}
+</script>
+
